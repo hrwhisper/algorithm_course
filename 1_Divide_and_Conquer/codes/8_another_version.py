c09b4294eecc51660aa427be9039b68ef959c036
hrwhisper <hrwhisper@qq.com>
2016-10-02 16:33:51 +0800

    commit homework 1

--- 1_Divide_and_Conquer/codes/8_another_version.py
+++ 1_Divide_and_Conquer/codes/8_another_version.py
@@ -3,65 +3,60 @@
 # @Author  : hrwhisper
 
 
-def merge_sort(L, R, a):
-    if L >= R - 1: return 0
-    mid = (L + R) >> 1
-    cnt = merge_sort(L, mid, a)
-    cnt += merge_sort(mid, R, a)
-    return cnt + merge(L, mid, R, a)
+def merge_sort(a):
+    if len(a) == 1: return a, 0
+    mid = len(a) >> 1
+    left, left_cnt = merge_sort(a[:mid])
+    right, right_cnt = merge_sort(a[mid:])
+    a, cnt = merge(left, right)
+    return a, left_cnt + right_cnt + cnt
 
 
-def merge(L, le, R, a):
+def merge(left, right):
     cnt = 0
-    rs = le
-    ls = L
     x = []
-    while ls < le and rs < R:
-        if a[ls] <= a[rs]:
-            x.append(a[ls])
-            ls += 1
+    left = left[::-1]
+    right = right[::-1]
+    while left or right:
+        if left and right and left[-1] <= right[-1]:
+            x.append(left.pop())
+        elif right:
+            x.append(right.pop())
+            cnt += len(left)
         else:
-            x.append(a[rs])
-            rs += 1
-            cnt += le - ls
-
-    for i in range(ls, le):
-        x.append(a[i])
-    for i in range(rs, R):
-        x.append(a[i])
-    for i in range(L, R):
-        a[i] = x[i - L]
-    return cnt
+            x.append(left.pop())
+    return x, cnt
 
 
-def quick_sort(L, R, a):
-    if L >= R: return 0
-    i, cnt = partition(L, R, a)
-    cnt += quick_sort(L, i - 1, a)
-    cnt += quick_sort(i + 1, R, a)
-    return cnt
+def find_inversions_by_merge_sort(a):
+    return merge_sort(a)[1]
 
 
-def partition(L, R, a):
+def quick_sort(a):
+    if not a: return 0
+    if len(a) == 1: return 0
+    i, cnt = partition(a)
+    left_cnt = quick_sort(a[:i])
+    right_cnt = quick_sort(a[i + 1:])
+    return left_cnt + right_cnt + cnt
+
+
+def partition(a):
+    if len(a) == 0: return 0, 0
     cnt = 0
     t = []
-    base = a[L]
-    i = L + 1
-    while i <= R:
+    base = a[0]
+    for i in range(len(a)):
         if a[i] < base:
-            cnt += i - L - len(t)
+            cnt += i - len(t)
             t.append(a[i])
-        i += 1
-    j = len(t) + L
-    t.append(base)  # or delete this line, and let i = L
-
-    for i in range(L + 1, R + 1):
+    j = len(t)
+    for i in range(len(a)):
         if base <= a[i]:
             t.append(a[i])
 
-    for i in range(L, R + 1):
-        a[i] = t[i - L]
-
+    for i in range(len(a)):
+        a[i] = t[i]
     return j, cnt
 
 
@@ -80,8 +75,8 @@ def test(test_cnt=1000, array_num=1000, L=1, R=100000):
     for i in range(test_cnt):
         A = [random.randint(L, R) for _ in range(random.randint(1, array_num))]
         # t = brute_inversions(A[:])
-        cnt_merge_sort = merge_sort(0, len(A), A[:])
-        cnt_quick_sort = quick_sort(0, len(A) - 1, A[:])
+        cnt_merge_sort = find_inversions_by_merge_sort(A[:])
+        cnt_quick_sort = quick_sort(A[:])
         if cnt_quick_sort != cnt_merge_sort:  # cnt_merge_sort != t or:
             print(A)
             print(cnt_merge_sort, cnt_quick_sort)
@@ -95,10 +90,10 @@ if __name__ == '__main__':
     with open('./Q8.txt') as f:
         a = list(map(int, f.read().split()))
     start = datetime.now()
-    print(merge_sort(0, len(a), a[:]))
+    print(find_inversions_by_merge_sort(a[:]))
     print('complete merge sort in {}s'.format((datetime.now() - start).total_seconds()))
     start = datetime.now()
-    print(quick_sort(0, len(a) - 1, a))
+    print(quick_sort(a[:]))
     print('complete quick sort in {}s'.format((datetime.now() - start).total_seconds()))
 
     test()
