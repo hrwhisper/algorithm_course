bfb7febfe99e42e4f8fba5c5d111136eb0a54348
hrwhisper <hrwhisper@qq.com>
2016-11-18 19:48:12 +0800

    update simplex algorithm. when the problem is infeasible, it will return none now.

--- 4_LP/7.py
+++ 4_LP/7.py
@@ -7,32 +7,24 @@ import numpy as np
 
 class Simplex(object):
     def __init__(self, obj):
-        self.mat = np.array([[0] + obj[:]])
+        self.mat = np.array([[0] + obj])
 
     def add_constraint(self, a, b):
         self.mat = np.vstack([self.mat, [b] + a])
 
-    def _find_theta(self, col):
-        return np.array([self.mat[i][0] / self.mat[i][col] if self.mat[i][col] > 0
-                         else 0x7fffffff for i in range(1, self.mat.shape[0])]).argmin() + 1
-
-    def _gaussian_elimination(self, row, col):
-        self.mat[row] /= self.mat[row][col]
-        for i in range(len(self.mat)):
-            if i != row:
-                self.mat[i] = self.mat[i] - self.mat[i][col] * self.mat[row]
-
     def solve(self):
-        n = len(self.mat) - 1  # to add slack varible
+        n = len(self.mat) - 1  # the number slack variables we should add
         temp = np.vstack([np.zeros((1, n)), np.eye(n)])  # add a diagonal array
-        self.mat = np.hstack([self.mat, temp])
-        # print(self.mat)
-        while self.mat[0].min() < 0:
-            col = self.mat[0].argmin()
-            row = self._find_theta(col)
-            self._gaussian_elimination(row, col)
-            print(row, col, '\n', self.mat)
-        return self.mat[0][0]
+        mat = self.mat = np.hstack([self.mat, temp])  # combine them!
+        while mat[0].min() < 0:
+            col = mat[0].argmin()
+            row = np.array([mat[i][0] / mat[i][col] if mat[i][col] > 0 else 0x7fffffff for i in
+                            range(1, mat.shape[0])]).argmin() + 1  # find the theta
+            if mat[row][col] <= 0: return None  # no answer. it equals the theta is ∞
+            mat[row] /= mat[row][col]
+            # for each i!= row do : self.mat[i] = self.mat[i] - self.mat[row] * self.mat[i][col]
+            mat[np.arange(mat.shape[0]) != row] -= mat[row] * mat[np.arange(mat.shape[0]) != row, col:col + 1]
+        return mat[0][0]
 
 
 if __name__ == '__main__':
@@ -40,9 +32,10 @@ if __name__ == '__main__':
          maximize z: 3*x1 + 2*x2;
             2*x1 + x2 <= 100;
             x1 + x2 <= 80;
+            answer :180
     """
     s = Simplex([-3, -2])
-    s.add_constraint([2, -1], 100)
+    s.add_constraint([2, 1], 100)
     s.add_constraint([1, 1], 80)
     s.add_constraint([1, 0], 40)
     print(s.solve())
@@ -55,6 +48,7 @@ if __name__ == '__main__':
        x + 2y + z <= 7
        z          <= 5
        x,y,z >= 0
+       answer :11
     """
     t = Simplex([-2, -3, -2])
     t.add_constraint([2, 1, 1], 4)
